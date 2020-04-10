95fd50580e57e969ee09dc0ba6d77195a0918499
rzrbld <razblade@gmail.com>
2020-04-10 22:49:32 +0300

    finalizing

--- config/config.go
+++ config/config.go
@@ -3,6 +3,7 @@ package config
 import (
 	"os"
 	strconv "strconv"
+  "strings"
 )
 
 var (
@@ -13,7 +14,8 @@ var (
 	MainHostPort   = getEnv("ZE3000_HOST_PORT", "localhost:8080")
 	MetricName     = getEnv("ZE3000_ZABBIX_METRIC_NAME", "zabbix_exporter_metric")
 	SourceRefresh  = getEnv("ZE3000_ZABBIX_REFRESH_DELAY_SEC", "10")
-	MetricLabels   = getEnv("ZE3000_ZABBIX_METRIC_LABELS", "name,key_,lastvalue,interfaces.ip,interfaces.dns")
+	MetricLabels   = strings.TrimSpace(getEnv("ZE3000_ZABBIX_METRIC_LABELS", "name,key_,hosts>name"))
+	// MetricLabels   = strings.TrimSpace(getEnv("ZE3000_ZABBIX_METRIC_LABELS", "name,key_,itemid"))
 	Query          = getEnv("ZE3000_ZABBIX_QUERY_PARAMS", `{     "jsonrpc": "2.0",     "method": "item.get",     "params": {     	"itemids":["330254","329514","178909"],         "output": ["name","key_","description","lastvalue"],         "selectHosts": ["name","status","host"],         "selectInterfaces": ["ip","dns"],         "sortfield":"key_"     },     "auth": "%auth-token%",     "id": 1 }`)
 )
 
--- handlers/metrics.go
+++ handlers/metrics.go
@@ -4,28 +4,50 @@ import (
 	"time"
   "encoding/json"
   "strconv"
+  "strings"
   "log"
+  // "fmt"
+  // "reflect"
 	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/promauto"
   zbx "github.com/rzrbld/zabbix-exporter-3000/zabbix"
   cnf "github.com/rzrbld/zabbix-exporter-3000/config"
 )
 
-var sourceRefreshSec, _ = strconv.Atoi(cnf.SourceRefresh)
-
-
-
-var objectsCount = promauto.NewGauge(prometheus.GaugeOpts{
-	Name: "objects_count_current",
-	Help: "number of objects on cluster",
-})
-
-var itemsMetric = promauto.NewGaugeVec(prometheus.GaugeOpts{
-	Name: cnf.MetricName,
-	Help: "bucket size in kbytes",
-}, []string{"name","key_","itemid"})
 
 func RecordMetrics() {
+
+  var sourceRefreshSec, _ = strconv.Atoi(cnf.SourceRefresh)
+  var labelsSliceRaw = strings.Split(cnf.MetricLabels, ",")
+  // labels in prom format
+  var labelsSlicePrometheus []string
+  // labels with path "a>b"
+  var labelsSliceComplex []string
+  // labels average
+  var labelsSliceAvg []string
+
+  for _, vl := range labelsSliceRaw{
+    if strings.Contains(vl, ">") {
+      labelsSlicePrometheus = append(labelsSlicePrometheus, strings.Replace(vl, ">", "_", -1))
+      labelsSliceComplex = append(labelsSliceComplex, vl)
+    }else{
+      labelsSlicePrometheus = append(labelsSlicePrometheus, vl)
+      labelsSliceAvg = append(labelsSliceAvg, vl)
+    }
+  }
+
+  log.Print("labels_prom   :", labelsSlicePrometheus)
+  log.Print("labels_complex:", labelsSliceComplex)
+  log.Print("labels_average:", labelsSliceAvg)
+
+  var itemsMetric = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+    Namespace: "our_company",
+  	Subsystem: "blob_storage",
+  	Name: cnf.MetricName,
+  	Help: "bucket size in kbytes",
+  }, labelsSlicePrometheus)
+
+  prometheus.MustRegister(itemsMetric)
+
 	go func() {
 		for {
       items, err := zbx.Session.Do(zbx.Query)
@@ -34,20 +56,44 @@ func RecordMetrics() {
       }else{
         var results []map[string]interface{}
         json.Unmarshal(items.Body, &results)
-        if err != nil {
-      		log.Print("ERROR While convert response to JSON: ",err)
-        }
 
-        for key, result := range results {
+        for _, result := range results {
 
-      		log.Print("Reading Value for Key :", key)
-      		// //Reading each value by its key
-      		// fmt.Println("Id :", result["itemid"],
-      		// 	"- Name :", result["name"],
-      		// 	"- Department :", result["key_"],
-      		// 	"- Designation :", result["hosts"])
+          // fmt.Println(reflect.TypeOf(labelsSlice))
+          // fmt.Println(reflect.TypeOf(result))
+          // fmt.Println(reflect.TypeOf(result["name"]))
+          // fmt.Println(reflect.TypeOf(result["interfaces"]))
+
+          labelsWithValues := make(map[string]string)
+
+
+          if len(labelsSliceAvg) > 0 {
+            for _, vAvg := range labelsSliceAvg{
+              labelsWithValues[vAvg] = result[vAvg].(string)
+            }
+          }
+
+          if len(labelsSliceComplex) > 0 {
+            for _, vCplx := range labelsSliceComplex{
+
+              var promLabel = strings.Replace(vCplx, ">", "_", -1)
+              var path = strings.Split(vCplx, ">")
+
+              log.Print("LENGTH >>>> ", len(result[path[0]].([]interface{})) )
+
+              if(len(result[path[0]].([]interface{})) > 0){
+                for _,cplx := range result[path[0]].([]interface{}) {
+                  subCplx := cplx.(map[string]interface{})
+      	          labelsWithValues[promLabel] = subCplx[path[1]].(string)
+      		      }
+              } else {
+                  labelsWithValues[promLabel] = "NA"
+              }
+            }
+          }
+
           f, _ := strconv.ParseFloat(result["lastvalue"].(string), 64)
-          itemsMetric.WithLabelValues(string(result["name"].(string)),string(result["key_"].(string)),string(result["itemid"].(string))).Set(float64(f))
+          itemsMetric.With(labelsWithValues).Set(float64(f))
       	}
       }
       time.Sleep(time.Duration(sourceRefreshSec) * time.Second)
